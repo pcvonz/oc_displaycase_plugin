e2c3db294f8236cb1c72c4113ca8e00cfd015e1e
pcvonz <pcvonz@gmail.com>
2017-09-24 10:16:48 -0700

    First commit

---
+++ lang/en/lang.php
@@ -0,0 +1,16 @@
+<?php return [
+    'plugin' => [
+        'name' => 'oc_agency_pages_plugin',
+        'description' => '',
+        'title' => 'Showcase',
+    ],
+    'item' => [
+        'title' => 'Title',
+        'description' => 'Description',
+        'tags' => 'Tags',
+        'image' => 'Image',
+        'categories' => 'Categories',
+        'slug' => 'Slug',
+        'manage_books' => 'Manage Books',
+    ],
+];
---
+++ updates/builder_table_create_paul_agencypages_data.php
@@ -0,0 +1,29 @@
+<?php namespace Paul\AgencyPages\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreatePaulAgencypagesData extends Migration
+{
+    public function up()
+    {
+        Schema::create('paul_agencypages_data', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('description')->nullable();
+            $table->string('tags')->nullable();
+            $table->binary('image');
+            $table->string('categories')->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('slug')->nullable();
+            $table->dateTime('date_added')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('paul_agencypages_data');
+    }
+}
