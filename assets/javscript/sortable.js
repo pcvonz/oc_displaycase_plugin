3a31e437c8c3b37b63197609921e797479a23d2e
pcvonz <pcvonz@gmail.com>
2017-09-29 23:44:52 -0700

    Sort order working

--- assets/javscript/sortable.js
+++ assets/javscript/sortable.js
@@ -1,18 +1,35 @@
 function initializeSorting() {
     var $tbody = $('.drag-handle').parents('table.data tbody');
-    Sortable.create($tbody[0], {
+    sort = Sortable.create($tbody[0], {
         handle: '.drag-handle',
         animation: 150,
         onEnd: function (evt) {
-            var $inputs = $(evt.from).find('td>div.drag-handle>input');
-            console.log($inputs[0].value);
+            var $inputs = $('.index_reposition').map(function () {
+              return $(this).val(); 
+            }).get();
             $.request('onReorder', {
-              data: {valuea: $inputs[0].value, valueb: $inputs[1].value},
+              data: {vals: $inputs},
+              success: initializeSorting()
             });
         }
     });
+
 }
 
-$(function () {
+
+$(window).ready(function (){
+  var sort;
+  var target = document.querySelector('#Items-update-RelationController-description');
+  console.log(target);
+  //observer stuff
+  initializeSorting();
+  var observer = new MutationObserver(function(mutations) {
     initializeSorting();
+  });
+
+  var config = { attributes: true, childList: true, characterData: true, subtree: true};
+  observer.observe(target, config);
+  
 });
+
+
--- controllers/Items.php
+++ controllers/Items.php
@@ -3,19 +3,18 @@
 use Backend\Classes\Controller;
 use BackendMenu;
 use Vonzimmerman\DisplayCase\Models\Description as Description;
-
+use Flash;
+use Event;
 class Items extends Controller
 {
     public $implement = [
         'Backend\Behaviors\ListController',
         'Backend\Behaviors\FormController',
         'Backend\Behaviors\RelationController',
-        'Backend\Behaviors\ReorderController'
    ];
     
-    public $reorderConfig = 'config_reorder.yaml';
     public $formConfig = 'config_item_form.yaml';
-    public $listConfig = ['items' => 'config_items_list.yaml', 'sections' => 'config_sections_list.yaml'];
+    public $listConfig = ['items' => 'config_items_list.yaml'];
     public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
@@ -24,12 +23,25 @@ class Items extends Controller
         $this->addJs("/plugins/vonzimmerman/displaycase/assets/javscript/sortable.js");
         parent::__construct();
         BackendMenu::setContext('Vonzimmerman.DisplayCase', 'main-menu-item', 'side-menu-item2');
+        // TODO: Sort the fields when the request is done
+        Event::listen('backend.list.extendQuery', function($widget, $field)
+        {
+        });
     }
     public function onReorder() 
     {
         $data = post();
-        $records = [$data['valuea'], $data['valueb']];
-        $model = new Description;
-        $model->setSortableOrder($records[0], $records[1]);
+        $records = $data['vals'];
+        $position = 0;
+        $moved = [];
+        foreach ($records as $id) {
+            if(in_array($id, $moved) || !$desc = Description::find($id))
+                continue;
+            $desc->sort_order = $position;
+            $desc->save();
+            $moved[]=$id;
+            $position++;
+        }
+        Flash::success('The list has been reordered');
     }
 }
